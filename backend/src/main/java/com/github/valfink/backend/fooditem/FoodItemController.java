8425110942878a4c721d8293c3915d10e26ad201
valfink <117605562+valfink@users.noreply.github.com>
2023-03-16 12:49:08 +0100

    Change annotation imports as I will use a lot of them

--- backend/src/main/java/com/github/valfink/backend/fooditem/FoodItemController.java
+++ backend/src/main/java/com/github/valfink/backend/fooditem/FoodItemController.java
@@ -1,9 +1,7 @@
 package com.github.valfink.backend.fooditem;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
