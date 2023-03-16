aa4e3192e60ef9305a9a7fcbccef76099fc1f2c4
valfink <117605562+valfink@users.noreply.github.com>
2023-03-16 16:08:00 +0100

    Add Route to gallery

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import FoodItemGallery from "./page/FoodItemGallery";
 
 function App() {
   return (
     <div className="App">
-      THIS WILL BE BANANA RESCUE!
+        <Routes>
+            <Route path={"/"} element={<Navigate to={"/food"} />} />
+            <Route path={"/food"} element={<FoodItemGallery />} />
+        </Routes>
     </div>
   );
 }
