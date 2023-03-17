32e54596e75ad88e0d1f2246ef5c538d7d059983
valfink <117605562+valfink@users.noreply.github.com>
2023-03-17 18:01:36 +0100

    Add Cookies.js and Axios interceptor for CSRF

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,30 @@ import './App.css';
 import {Navigate, Route, Routes} from "react-router-dom";
 import FoodItemGallery from "./page/FoodItemGallery";
 import UserSignUpPage from "./page/UserSignUpPage";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+axios.interceptors.request.use(config => {
+    if (["put", "post", "delete"].includes(config.method || "")) {
+        return fetch("/api/csrf")
+            .then(() => {
+                config.headers["X-XSRF-TOKEN"] = Cookies.get("XSRF-TOKEN");
+                return config;
+            });
+    } else {
+        return config;
+    }
+}, error => Promise.reject(error));
 
 function App() {
-  return (
-    <div className="App">
-        <Routes>
-            <Route path={"/"} element={<Navigate to={"/food"} />} />
-            <Route path={"/food"} element={<FoodItemGallery/>}/>
-            <Route path={"/signup"} element={<UserSignUpPage/>}/>
-        </Routes>
-    </div>
+    return (
+        <div className="App">
+            <Routes>
+                <Route path={"/"} element={<Navigate to={"/food"}/>}/>
+                <Route path={"/food"} element={<FoodItemGallery/>}/>
+                <Route path={"/signup"} element={<UserSignUpPage/>}/>
+            </Routes>
+        </div>
   );
 }
 
