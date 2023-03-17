5c2d2e9ae0028f3089f9b906026938e5085ac8bc
valfink <117605562+valfink@users.noreply.github.com>
2023-03-17 14:39:31 +0100

    Name package util instead of config

--- backend/src/main/java/com/github/valfink/backend/util/SecurityConfig.java
+++ backend/src/main/java/com/github/valfink/backend/util/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.github.valfink.backend.config;
+package com.github.valfink.backend.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
