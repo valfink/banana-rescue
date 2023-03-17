6e1a7465f0a20726e4ef268981169a54de75b70c
valfink <117605562+valfink@users.noreply.github.com>
2023-03-17 14:08:22 +0100

    Add @Document and @Id annotations

--- backend/src/main/java/com/github/valfink/backend/fooditem/FoodItem.java
+++ backend/src/main/java/com/github/valfink/backend/fooditem/FoodItem.java
@@ -1,8 +1,13 @@
 package com.github.valfink.backend.fooditem;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.Instant;
 
+@Document("foodItems")
 public record FoodItem(
+        @Id
         String id,
         String title,
         String photo_uri,
