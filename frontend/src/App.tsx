e711eecd019b63ce045f0c8472c0ebe187ff682e
valfink <117605562+valfink@users.noreply.github.com>
2023-03-17 17:48:56 +0100

    Add User signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import {Navigate, Route, Routes} from "react-router-dom";
 import FoodItemGallery from "./page/FoodItemGallery";
+import UserSignUpPage from "./page/UserSignUpPage";
 
 function App() {
   return (
     <div className="App">
         <Routes>
             <Route path={"/"} element={<Navigate to={"/food"} />} />
-            <Route path={"/food"} element={<FoodItemGallery />} />
+            <Route path={"/food"} element={<FoodItemGallery/>}/>
+            <Route path={"/signup"} element={<UserSignUpPage/>}/>
         </Routes>
     </div>
   );
