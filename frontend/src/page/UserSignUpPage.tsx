7a9aafe3c6a1652be2cf1c7db527a86d6edb24a4
valfink <117605562+valfink@users.noreply.github.com>
2023-03-20 15:53:52 +0100

    Change UserForm to work for signup and login

--- frontend/src/page/UserSignUpPage.tsx
+++ frontend/src/page/UserSignUpPage.tsx
@@ -5,7 +5,7 @@ export default function UserSignUpPage() {
     return (
         <main className={"user-form"}>
             <h1>Create a new account</h1>
-            <UserForm/>
+            <UserForm action={"signup"}/>
         </main>
     );
 }
