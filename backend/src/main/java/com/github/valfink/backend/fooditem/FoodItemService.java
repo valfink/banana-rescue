95f8636b0ee25af3c17ee928cea461654f58e70e
valfink <117605562+valfink@users.noreply.github.com>
2023-03-20 18:01:55 +0100

    Add addFoodItem method

--- backend/src/main/java/com/github/valfink/backend/fooditem/FoodItemService.java
+++ backend/src/main/java/com/github/valfink/backend/fooditem/FoodItemService.java
@@ -1,9 +1,12 @@
 package com.github.valfink.backend.fooditem;
 
 import com.github.valfink.backend.mongouser.MongoUserService;
+import com.github.valfink.backend.util.IdService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
+import java.util.InputMismatchException;
 import java.util.List;
 
 @Service
@@ -11,6 +14,7 @@ import java.util.List;
 public class FoodItemService {
     private final FoodItemRepository foodItemRepository;
     private final MongoUserService mongoUserService;
+    private final IdService idService;
 
     public List<FoodItemDTOResponse> getAllFoodItems() {
         return foodItemRepository.findAll()
@@ -26,5 +30,44 @@ public class FoodItemService {
                         item.description()
                 ))
                 .toList();
+    }
+
+    public FoodItemDTOResponse addFoodItem(FoodItemDTORequest foodItemDTORequest, Principal principal) {
+        if (foodItemDTORequest.title() == null || foodItemDTORequest.title().isBlank()) {
+            throw new InputMismatchException("Title must not be blank");
+        }
+        if (foodItemDTORequest.location() == null || foodItemDTORequest.location().isBlank()) {
+            throw new InputMismatchException("Location must not be blank");
+        }
+        if (foodItemDTORequest.pickup_until() == null) {
+            throw new InputMismatchException("Pickup until must not be blank");
+        }
+        if (foodItemDTORequest.consume_until() == null) {
+            throw new InputMismatchException("Consume until must not be blank");
+        }
+        if (foodItemDTORequest.description() == null || foodItemDTORequest.description().isBlank()) {
+            throw new InputMismatchException("Description must not be blank");
+        }
+        FoodItem foodItem = foodItemRepository.save(new FoodItem(
+                idService.generateId(),
+                mongoUserService.getMongoUserDTOResponseByUsername(principal.getName()).id(),
+                foodItemDTORequest.title(),
+                // TODO: PHOTO UPLOAD!
+                "PHOTO URI",
+                foodItemDTORequest.location(),
+                foodItemDTORequest.pickup_until(),
+                foodItemDTORequest.consume_until(),
+                foodItemDTORequest.description()
+        ));
+        return new FoodItemDTOResponse(
+                foodItem.id(),
+                mongoUserService.getMongoUserDTOResponseById(foodItem.donator_id()),
+                foodItem.title(),
+                foodItem.photo_uri(),
+                foodItem.location(),
+                foodItem.pickup_until(),
+                foodItem.consume_until(),
+                foodItem.description()
+        );
     }
 }
--- backend/src/test/java/com/github/valfink/backend/fooditem/FoodItemServiceTest.java
+++ backend/src/test/java/com/github/valfink/backend/fooditem/FoodItemServiceTest.java
@@ -2,34 +2,43 @@ package com.github.valfink.backend.fooditem;
 
 import com.github.valfink.backend.mongouser.MongoUserDTOResponse;
 import com.github.valfink.backend.mongouser.MongoUserService;
+import com.github.valfink.backend.util.IdService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.security.Principal;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 class FoodItemServiceTest {
     FoodItemRepository foodItemRepository;
     MongoUserService mongoUserService;
+    IdService idService;
     FoodItemService foodItemService;
+    Principal principal;
     FoodItem foodItem1;
     FoodItemDTOResponse foodItemDTOResponse1;
+    MongoUserDTOResponse mongoUserDTOResponse1;
 
     @BeforeEach
     void setUp() {
         foodItemRepository = mock(FoodItemRepository.class);
         mongoUserService = mock(MongoUserService.class);
-        foodItemService = new FoodItemService(foodItemRepository, mongoUserService);
+        idService = mock(IdService.class);
+        principal = mock(Principal.class);
+        foodItemService = new FoodItemService(foodItemRepository, mongoUserService, idService);
+        mongoUserDTOResponse1 = new MongoUserDTOResponse("1", "user");
         foodItem1 = new FoodItem(
                 "1",
-                "1",
+                mongoUserDTOResponse1.id(),
                 "Food Item 1",
-                "https://photo.com/1.jpg",
+                "PHOTO URI",
                 "Berlin",
                 Instant.parse("2023-03-16T11:14:00Z"),
                 Instant.parse("2023-03-18T11:00:00Z"),
@@ -37,7 +46,7 @@ class FoodItemServiceTest {
         );
         foodItemDTOResponse1 = new FoodItemDTOResponse(
                 foodItem1.id(),
-                new MongoUserDTOResponse(foodItem1.donator_id(), "user"),
+                mongoUserDTOResponse1,
                 foodItem1.title(),
                 foodItem1.photo_uri(),
                 foodItem1.location(),
@@ -59,5 +68,54 @@ class FoodItemServiceTest {
 
         // THEN
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void addFoodItem_whenValidDTORequest_thenReturnDTOResponse() {
+        // GIVEN
+        FoodItemDTORequest foodItemDTORequest = new FoodItemDTORequest(foodItem1.title(), foodItem1.location(), foodItem1.pickup_until(), foodItem1.consume_until(), foodItem1.description());
+        when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
+        when(idService.generateId()).thenReturn("1");
+        when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
+        when(foodItemRepository.save(foodItem1)).thenReturn(foodItem1);
+        when(mongoUserService.getMongoUserDTOResponseById(mongoUserDTOResponse1.id())).thenReturn(mongoUserDTOResponse1);
+
+        // WHEN
+        FoodItemDTOResponse expected = foodItemDTOResponse1;
+        FoodItemDTOResponse actual = foodItemService.addFoodItem(foodItemDTORequest, principal);
+
+        // THEN
+        verify(foodItemRepository).save(foodItem1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addFoodItem_whenNoTitle_thenThrowException() {
+        // WHEN & THEN
+        assertThrows(InputMismatchException.class, () -> foodItemService.addFoodItem(new FoodItemDTORequest(null, foodItem1.location(), foodItem1.pickup_until(), foodItem1.consume_until(), foodItem1.description()), principal));
+    }
+
+    @Test
+    void addFoodItem_whenNoLocation_thenThrowException() {
+        // WHEN & THEN
+        assertThrows(InputMismatchException.class, () -> foodItemService.addFoodItem(new FoodItemDTORequest(foodItem1.title(), "", foodItem1.pickup_until(), foodItem1.consume_until(), foodItem1.description()), principal));
+    }
+
+    @Test
+    void addFoodItem_whenNoPickupUntil_thenThrowException() {
+        // WHEN & THEN
+        assertThrows(InputMismatchException.class, () -> foodItemService.addFoodItem(new FoodItemDTORequest(foodItem1.title(), foodItem1.location(), null, foodItem1.consume_until(), foodItem1.description()), principal));
+    }
+
+    @Test
+    void addFoodItem_whenNoConsumeUntil_thenThrowException() {
+        // WHEN & THEN
+        assertThrows(InputMismatchException.class, () -> foodItemService.addFoodItem(new FoodItemDTORequest(foodItem1.title(), foodItem1.location(), foodItem1.pickup_until(), null, foodItem1.description()), principal));
+    }
+
+    @Test
+    void addFoodItem_whenNoDescription_thenThrowException() {
+        // WHEN & THEN
+        assertThrows(InputMismatchException.class, () -> foodItemService.addFoodItem(new FoodItemDTORequest(foodItem1.title(), foodItem1.location(), foodItem1.pickup_until(), foodItem1.consume_until(), "   "), principal));
     }
 }
