89d1d7b39250362fc96ad685160d3f5fdb787ef1
valfink <117605562+valfink@users.noreply.github.com>
2023-03-20 09:36:36 +0100

    Change return type to Optional

--- backend/src/main/java/com/github/valfink/backend/mongouser/MongoUserRepository.java
+++ backend/src/main/java/com/github/valfink/backend/mongouser/MongoUserRepository.java
@@ -3,9 +3,11 @@ package com.github.valfink.backend.mongouser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MongoUserRepository extends MongoRepository<MongoUser, String> {
     boolean existsMongoUserByUsername(String username);
 
-    MongoUser findMongoUserByUsername(String username);
+    Optional<MongoUser> findMongoUserByUsername(String username);
 }
