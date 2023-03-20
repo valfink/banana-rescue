d85f5457185a1b9a1ee5aa81f2a61b8a9adb6e9f
valfink <117605562+valfink@users.noreply.github.com>
2023-03-20 15:54:06 +0100

    Add UserLoginPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import FoodItemGallery from "./page/FoodItemGallery";
 import UserSignUpPage from "./page/UserSignUpPage";
 import axios from "axios";
 import Cookies from "js-cookie";
+import UserLogInPage from "./page/UserLogInPage";
 
 axios.interceptors.request.use(config => {
     if (["put", "post", "delete"].includes(config.method || "")) {
@@ -25,6 +26,7 @@ function App() {
                 <Route path={"/"} element={<Navigate to={"/food"}/>}/>
                 <Route path={"/food"} element={<FoodItemGallery/>}/>
                 <Route path={"/signup"} element={<UserSignUpPage/>}/>
+                <Route path={"/login"} element={<UserLogInPage/>}/>
             </Routes>
         </div>
   );
---
+++ frontend/src/page/UserLogInPage.tsx
@@ -0,0 +1,10 @@
+import UserForm from "../component/UserForm";
+
+export default function UserLogInPage() {
+    return (
+        <main className={"user-form"}>
+            <h1>Log In</h1>
+            <UserForm action={"login"}/>
+        </main>
+    );
+}
