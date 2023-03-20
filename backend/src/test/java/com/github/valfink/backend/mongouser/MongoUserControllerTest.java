bffe6a72f40d21873458b0a0e67f668f5c482e0e
valfink <117605562+valfink@users.noreply.github.com>
2023-03-20 12:03:56 +0100

    Encode password and test login

--- backend/src/test/java/com/github/valfink/backend/mongouser/MongoUserControllerTest.java
+++ backend/src/test/java/com/github/valfink/backend/mongouser/MongoUserControllerTest.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -21,11 +23,13 @@ class MongoUserControllerTest {
     MockMvc mockMvc;
     @Autowired
     MongoUserRepository mongoUserRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     MongoUser mongoUser1;
 
     @BeforeEach
     void setUp() {
-        mongoUser1 = new MongoUser("1", "user", "pass", "BASIC");
+        mongoUser1 = new MongoUser("1", "user", passwordEncoder.encode("password"), "BASIC");
     }
 
     @Test
@@ -90,6 +94,31 @@ class MongoUserControllerTest {
     @DirtiesContext
     void getMe_whenNotAuthenticated_thenReturn401() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DirtiesContext
+    void login_whenUserExists_thenReturnUserInfos() throws Exception {
+        mongoUserRepository.save(mongoUser1);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users/login")
+                        .with(httpBasic("user", "password"))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                        "id": "1",
+                        "username": "user"
+                        }
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    void login_whenUsernameIsNotInRepo_thenReturn401() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users/login")
+                        .with(httpBasic("user", "password"))
+                        .with(csrf()))
                 .andExpect(status().isUnauthorized());
     }
 }
