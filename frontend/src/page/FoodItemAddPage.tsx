57094eeca8e989b30c34acc0de3cdc1c16e32a78
valfink <117605562+valfink@users.noreply.github.com>
2023-03-21 14:26:00 +0100

    Add "add foodItem" page

---
+++ frontend/src/page/FoodItemAddPage.tsx
@@ -0,0 +1,10 @@
+import FoodItemForm from "../component/FoodItemForm";
+
+export default function FoodItemAddPage() {
+    return (
+        <main className={"form"}>
+            <h1>Add a new Item</h1>
+            <FoodItemForm/>
+        </main>
+    );
+}
