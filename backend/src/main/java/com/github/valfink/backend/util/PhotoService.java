9f7b035835b0c98e25d47e8c2114e1c64b17f906
valfink <117605562+valfink@users.noreply.github.com>
2023-03-21 15:56:36 +0100

    Add PhotoService

---
+++ backend/src/main/java/com/github/valfink/backend/util/PhotoService.java
@@ -0,0 +1,26 @@
+package com.github.valfink.backend.util;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhotoService {
+    private final Cloudinary cloudinary;
+
+    public PhotoService() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"));
+        config.put("api_key", System.getenv("CLOUDINARY_API_KEY"));
+        config.put("api_secret", System.getenv("CLOUDINARY_API_SECRET"));
+        cloudinary = new Cloudinary(config);
+    }
+
+    public String uploadPhoto(MultipartFile photo) throws IOException {
+        Map result = cloudinary.uploader().upload(photo.getBytes(), ObjectUtils.emptyMap());
+        return result.get("url").toString();
+    }
+}
