8d93e59a0a6cb12dea4459e3c29263dd56577df9
valfink <117605562+valfink@users.noreply.github.com>
2023-03-21 17:12:56 +0100

    Add @Service annotation

--- backend/src/main/java/com/github/valfink/backend/util/PhotoService.java
+++ backend/src/main/java/com/github/valfink/backend/util/PhotoService.java
@@ -2,12 +2,14 @@ package com.github.valfink.backend.util;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class PhotoService {
     private final Cloudinary cloudinary;
 
