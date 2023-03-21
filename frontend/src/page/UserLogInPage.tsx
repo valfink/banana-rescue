adc4a121edee817860821cb20ec790c1264e5a87
valfink <117605562+valfink@users.noreply.github.com>
2023-03-21 11:49:13 +0100

    Move styling to App.css because it is valid for all forms

--- frontend/src/page/UserLogInPage.tsx
+++ frontend/src/page/UserLogInPage.tsx
@@ -2,7 +2,7 @@ import UserForm from "../component/UserForm";
 
 export default function UserLogInPage() {
     return (
-        <main className={"user-form"}>
+        <main className={"form"}>
             <h1>Log In</h1>
             <UserForm action={"login"}/>
         </main>
