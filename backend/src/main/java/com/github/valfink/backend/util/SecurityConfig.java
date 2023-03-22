8ecc586bf43aa0a4f580f9c6462ecce645e46dba
valfink <117605562+valfink@users.noreply.github.com>
2023-03-22 17:02:21 +0100

    Permit requests to specific food items

--- backend/src/main/java/com/github/valfink/backend/util/SecurityConfig.java
+++ backend/src/main/java/com/github/valfink/backend/util/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .sessionManagement(config ->
                         config.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/api/food").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/food/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/csrf").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/users/login").permitAll()
