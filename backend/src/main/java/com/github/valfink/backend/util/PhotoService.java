ef2f5a6159e806bc34730d72cceb9cb03c1b9a43
valfink <117605562+valfink@users.noreply.github.com>
2023-03-22 16:23:36 +0100

    Change Cloudinary implementation to make it mock-testable

--- backend/src/main/java/com/github/valfink/backend/util/PhotoService.java
+++ backend/src/main/java/com/github/valfink/backend/util/PhotoService.java
@@ -2,24 +2,18 @@ package com.github.valfink.backend.util;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class PhotoService {
     private final Cloudinary cloudinary;
 
-    public PhotoService(@Value("${cloudinary.cloud.name}") String cloudName, @Value("${cloudinary.api.key}") String apiKey, @Value("${cloudinary.api.secret}") String apiSecret) {
-        Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", cloudName);
-        config.put("api_key", apiKey);
-        config.put("api_secret", apiSecret);
-        cloudinary = new Cloudinary(config);
+    public PhotoService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
     }
 
     public String uploadPhoto(MultipartFile photo) throws IOException {
