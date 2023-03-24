8b947c605d96942a716c85714d7938604003738c
valfink <117605562+valfink@users.noreply.github.com>
2023-03-24 16:26:50 +0100

    Combine HeaderBar and FullScreenNav

--- frontend/src/component/HeaderBar.tsx
+++ frontend/src/component/HeaderBar.tsx
@@ -1,23 +1,28 @@
 import "./HeaderBar.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCircleUser as loggedInUser} from "@fortawesome/free-solid-svg-icons";
+import {faBars, faCircleUser as loggedInUser, faTruckMedical} from "@fortawesome/free-solid-svg-icons";
 import {faCircleUser as loggedOutUser} from "@fortawesome/free-regular-svg-icons";
-import {useContext} from "react";
+import React, {useContext} from "react";
 import {UserContext, UserContextType} from "../context/UserContext";
 import {Link} from "react-router-dom";
 
 type HeaderBarProps = {
     displayShadow: boolean;
+    setShowNavBar: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function HeaderBar(props: HeaderBarProps) {
     const {user} = useContext(UserContext) as UserContextType;
 
+    function handleShowNavBarClick() {
+        props.setShowNavBar(showNavBar => !showNavBar);
+    }
+
     return (
-        <header className={"header-bar" + (props.displayShadow ? " with-shadow" : "")}>
-            <span>...</span>
-            Banana Rescue
-            <Link to={"/login"}><span><FontAwesomeIcon icon={user ? loggedInUser : loggedOutUser}/></span></Link>
-        </header>
+        <section className={"header-bar" + (props.displayShadow ? " with-shadow" : "")}>
+            <span className={"cursor-pointer"} onClick={handleShowNavBarClick}><FontAwesomeIcon icon={faBars}/></span>
+            <span><FontAwesomeIcon icon={faTruckMedical}/> Banana Rescue</span>
+            <Link to={"/login"}><FontAwesomeIcon icon={user ? loggedInUser : loggedOutUser}/></Link>
+        </section>
     );
 }
