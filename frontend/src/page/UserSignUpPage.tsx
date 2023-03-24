15c1f835416452392e29ce6b2299d0e1921cea69
valfink <117605562+valfink@users.noreply.github.com>
2023-03-24 17:13:14 +0100

    Link both user forms

--- frontend/src/page/UserSignUpPage.tsx
+++ frontend/src/page/UserSignUpPage.tsx
@@ -1,10 +1,19 @@
 import UserForm from "../component/UserForm";
+import {Link, useLocation} from "react-router-dom";
 
 export default function UserSignUpPage() {
+    const location = useLocation();
+
     return (
-        <main className={"form"}>
+        <main className={"user form"}>
             <h1>Create a new account</h1>
             <UserForm action={"signup"}/>
+            <footer>
+                Already have an account?<br/>
+                <Link to={"/login"} state={{navBarBackLink: location.pathname, oldState: location.state}}>
+                    Log in
+                </Link>
+            </footer>
         </main>
     );
 }
