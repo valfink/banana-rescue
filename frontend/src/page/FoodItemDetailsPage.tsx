03c634ee01736b02139b7a893a5082cae8d82bb2
valfink <117605562+valfink@users.noreply.github.com>
2023-03-24 09:52:08 +0100

    Add link to edit item if appropriate

--- frontend/src/page/FoodItemDetailsPage.tsx
+++ frontend/src/page/FoodItemDetailsPage.tsx
@@ -1,15 +1,17 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {useContext, useEffect, useState} from "react";
 import {SetAppIsLoadingContext} from "../context/SetAppIsLoadingContext";
 import "./FoodItemDetailsPage.css";
 import moment from "moment/moment";
 import {fetchSingleFoodItem} from "../util/foodItemRequests";
 import {FoodItem} from "../model/FoodItem";
+import {UserContext} from "../context/UserContext";
 
 export default function FoodItemDetailsPage() {
     const {id} = useParams();
     const setAppIsLoading = useContext(SetAppIsLoadingContext);
     const [foodItem, setFoodItem] = useState<FoodItem | undefined>(undefined);
+    const {user} = useContext(UserContext);
 
     useEffect(() => {
         fetchSingleFoodItem(id, setAppIsLoading)
@@ -28,16 +30,20 @@ export default function FoodItemDetailsPage() {
     return (
         <section className={"food-item-details"}>
             {foodItem.photoUri && <header style={{backgroundImage: `url(${foodItem.photoUri})`}}/>}
-            <main>
-                <h1>{foodItem.title}</h1>
-                <ul>
-                    <li><strong>Pickup until:</strong> {moment(foodItem.pickupUntil).calendar()}</li>
-                    <li><strong>Consume within:</strong> {moment(foodItem.consumeUntil).fromNow(true)}</li>
-                    <li><strong>Location:</strong> {foodItem.location}</li>
-                    <li><strong>Donator:</strong> {foodItem.donator.username}</li>
-                    <li><strong>Comment:</strong> {foodItem.description}</li>
-                </ul>
-            </main>
+            <section>
+                <main>
+                    <h1>{foodItem.title}</h1>
+                    <ul>
+                        <li><strong>Pickup until:</strong> {moment(foodItem.pickupUntil).calendar()}</li>
+                        <li><strong>Consume within:</strong> {moment(foodItem.consumeUntil).fromNow(true)}</li>
+                        <li><strong>Location:</strong> {foodItem.location}</li>
+                        <li><strong>Donator:</strong> {foodItem.donator.username}</li>
+                        <li><strong>Comment:</strong> {foodItem.description}</li>
+                    </ul>
+                </main>
+                {user?.id === foodItem.donator.id &&
+                    <Link to={`/food/${foodItem.id}/edit`} className={"primary-button"}>Edit Item</Link>}
+            </section>
         </section>
     );
 }
