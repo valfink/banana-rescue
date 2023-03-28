0bdeb7a4cae78fa74c2ba9d5ee636eb0570ccb35
valfink <117605562+valfink@users.noreply.github.com>
2023-03-28 09:18:58 +0200

    Add annotations

--- backend/src/main/java/com/github/valfink/backend/chat/Chat.java
+++ backend/src/main/java/com/github/valfink/backend/chat/Chat.java
@@ -1,6 +1,11 @@
 package com.github.valfink.backend.chat;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("chats")
 public record Chat(
+        @Id
         String id,
         String donatorId,
         String candidateId,
--- backend/src/main/java/com/github/valfink/backend/chat/ChatMessage.java
+++ backend/src/main/java/com/github/valfink/backend/chat/ChatMessage.java
@@ -1,8 +1,13 @@
 package com.github.valfink.backend.chat;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.Instant;
 
+@Document("chatMessages")
 public record ChatMessage(
+        @Id
         String id,
         String chatId,
         String senderId,
