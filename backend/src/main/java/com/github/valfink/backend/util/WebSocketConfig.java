7b5acfda1d51db8cbfba25a59a03af5cdeed981a
valfink <117605562+valfink@users.noreply.github.com>
2023-03-29 13:54:13 +0200

    Change WebSocketConfig to use STOMP

--- backend/src/main/java/com/github/valfink/backend/util/WebSocketConfig.java
+++ backend/src/main/java/com/github/valfink/backend/util/WebSocketConfig.java
@@ -1,22 +1,28 @@
 package com.github.valfink.backend.util;
 
-import com.github.valfink.backend.chat.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final ChatService chatService;
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        // TODO: Authentifizierung bei Subscription auf Topic!
+        config.setApplicationDestinationPrefixes("/app");
+    }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
-                .addHandler(chatService, "/api/ws/chat")
+                .addEndpoint("/api/ws")
                 .setAllowedOrigins("*");
+        // .withSockJS(); ?
     }
 }
