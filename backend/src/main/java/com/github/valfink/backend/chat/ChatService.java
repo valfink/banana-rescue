f9fce2c0dc955a848142f59fde9f0d23ed00ed42
valfink <117605562+valfink@users.noreply.github.com>
2023-03-29 14:46:57 +0200

    Add addMessageAndSendIntoChat method

--- backend/src/main/java/com/github/valfink/backend/chat/ChatService.java
+++ backend/src/main/java/com/github/valfink/backend/chat/ChatService.java
@@ -5,6 +5,7 @@ import com.github.valfink.backend.fooditem.FoodItemService;
 import com.github.valfink.backend.mongouser.MongoUserDTOResponse;
 import com.github.valfink.backend.mongouser.MongoUserService;
 import com.github.valfink.backend.util.IdService;
+import com.github.valfink.backend.util.TimestampService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +19,24 @@ public class ChatService {
     private final MongoUserService mongoUserService;
     private final FoodItemService foodItemService;
     private final IdService idService;
+    private final TimestampService timestampService;
 
-    private ChatDTOResponse chatDTOResponseFromChatAndFoodItem(Chat chat, FoodItemDTOResponse foodItem) {
+    private Chat checkIfUserIsInChatAndReturnChat(String chatId, MongoUserDTOResponse user) {
+        Chat chat = chatRepository.findById(chatId)
+                .orElseThrow(() -> new ChatExceptionNotFound("The chat with the id " + chatId + " does not exist."));
+        FoodItemDTOResponse foodItem = foodItemService.getFoodItemById(chat.foodItemId());
+
+        if (!foodItem.donator().id().equals(user.id()) && !chat.candidateId().equals(user.id())) {
+            throw new ChatExceptionAuthorization("You are not participant in this chat!");
+        }
+
+        return chat;
+    }
+
+    private ChatDTOResponse chatDTOResponseFromChat(Chat chat) {
         return new ChatDTOResponse(
                 chat.id(),
-                foodItem,
+                foodItemService.getFoodItemById(chat.foodItemId()),
                 mongoUserService.getMongoUserDTOResponseById(chat.candidateId()),
                 chatMessageRepository.getChatMessagesByChatId(chat.id())
         );
@@ -48,14 +62,22 @@ public class ChatService {
 
     public ChatDTOResponse getChatById(String chatId, Principal principal) {
         MongoUserDTOResponse user = mongoUserService.getMongoUserDTOResponseByUsername(principal.getName());
-        Chat chat = chatRepository.findById(chatId)
-                .orElseThrow(() -> new ChatExceptionNotFound("The chat with the id " + chatId + " does not exist."));
-        FoodItemDTOResponse foodItem = foodItemService.getFoodItemById(chat.foodItemId());
+        Chat chat = checkIfUserIsInChatAndReturnChat(chatId, user);
 
-        if (!foodItem.donator().id().equals(user.id()) && !chat.candidateId().equals(user.id())) {
-            throw new ChatExceptionAuthorization("You are not participant in this chat!");
-        }
+        return chatDTOResponseFromChat(chat);
+    }
 
-        return chatDTOResponseFromChatAndFoodItem(chat, foodItem);
+    public ChatMessage addMessageAndSendIntoChat(String message, String chatId, Principal principal) {
+        MongoUserDTOResponse user = mongoUserService.getMongoUserDTOResponseByUsername(principal.getName());
+        checkIfUserIsInChatAndReturnChat(chatId, user);
+
+        ChatMessage chatMessage = new ChatMessage(
+                idService.generateId(),
+                chatId,
+                user.id(),
+                timestampService.generateTimestamp(),
+                message);
+
+        return chatMessageRepository.save(chatMessage);
     }
 }
