2715ccde1d8ad36ce602560bc7973277cb9f8854
valfink <117605562+valfink@users.noreply.github.com>
2023-03-30 22:11:42 +0200

    Make chats sortable by last update

--- backend/src/main/java/com/github/valfink/backend/chat/ChatDTOResponse.java
+++ backend/src/main/java/com/github/valfink/backend/chat/ChatDTOResponse.java
@@ -3,6 +3,8 @@ package com.github.valfink.backend.chat;
 import com.github.valfink.backend.fooditem.FoodItemDTOResponse;
 import com.github.valfink.backend.mongouser.MongoUserDTOResponse;
 
+import java.time.Instant;
+import java.util.Comparator;
 import java.util.List;
 
 public record ChatDTOResponse(
@@ -11,5 +13,11 @@ public record ChatDTOResponse(
         MongoUserDTOResponse candidate,
         List<ChatMessage> messages
 ) {
+    Instant getLastUpdate() {
+        return messages.stream()
+                .map(ChatMessage::timestamp)
+                .max(Comparator.naturalOrder())
+                .orElse(Instant.now());
+    }
 }
 
