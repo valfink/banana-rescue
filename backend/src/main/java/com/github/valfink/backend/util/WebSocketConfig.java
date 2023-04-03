65def96ea681c4af700537f2862a467a164ef0c1
valfink <117605562+valfink@users.noreply.github.com>
2023-04-03 12:47:24 +0200

    Restore app destination prefix

--- backend/src/main/java/com/github/valfink/backend/util/WebSocketConfig.java
+++ backend/src/main/java/com/github/valfink/backend/util/WebSocketConfig.java
@@ -14,6 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/queue");
+        config.setApplicationDestinationPrefixes("/api/ws");
     }
 
     @Override
