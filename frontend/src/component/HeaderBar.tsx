de3d2249cc62edf9ca05deb087aa1a72c6345c6f
valfink <117605562+valfink@users.noreply.github.com>
2023-04-04 16:01:31 +0200

    Add logout warning screen

--- frontend/src/component/HeaderBar.tsx
+++ frontend/src/component/HeaderBar.tsx
@@ -2,9 +2,10 @@ import "./HeaderBar.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowLeft, faBars, faCircleUser as loggedInUser, faTruckMedical} from "@fortawesome/free-solid-svg-icons";
 import {faCircleUser as loggedOutUser} from "@fortawesome/free-regular-svg-icons";
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {UserContext, UserContextType} from "../context/UserContext";
 import {Link, useLocation} from "react-router-dom";
+import LogOutWarningScreen from "../modal/LogOutWarningScreen";
 
 type HeaderBarProps = {
     displayShadow: boolean;
@@ -13,6 +14,7 @@ type HeaderBarProps = {
 
 export default function HeaderBar(props: HeaderBarProps) {
     const {user, logOutUser} = useContext(UserContext) as UserContextType;
+    const [showLogOutWarning, setShowLogOutWarning] = useState(false);
     const location = useLocation();
     const navBarBackLink = location.state?.navBarBackLink;
 
@@ -24,15 +26,32 @@ export default function HeaderBar(props: HeaderBarProps) {
         props.setShowNavBar(false);
     }
 
+    function handleUserIconClick() {
+        setShowLogOutWarning(true);
+    }
+
+    function handleCloseLogOutWarningClick() {
+        setShowLogOutWarning(false);
+    }
+
+    function handleLogOutClick() {
+        logOutUser()
+            .finally(() => {
+                setShowLogOutWarning(false);
+            });
+    }
+
     return (
         <section className={"header-bar" + (props.displayShadow ? " with-shadow" : "")}>
+            <LogOutWarningScreen modalIsOpen={showLogOutWarning} closeModal={handleCloseLogOutWarningClick}
+                                 logOut={handleLogOutClick}/>
             {navBarBackLink
                 ? <Link to={navBarBackLink} state={location.state.oldState}><FontAwesomeIcon icon={faArrowLeft}/></Link>
                 : <span className={"cursor-pointer"} onClick={handleShowNavBarClick}><FontAwesomeIcon
                     icon={faBars}/></span>}
             <Link to={"/"} onClick={closeNavBarIfOpen}><FontAwesomeIcon icon={faTruckMedical}/> Banana Rescue</Link>
             {user ?
-                <span onClick={logOutUser} className={"cursor-pointer"}>
+                <span onClick={handleUserIconClick} className={"cursor-pointer"}>
                     <FontAwesomeIcon icon={loggedInUser}/>
                 </span>
                 :
---
+++ frontend/src/modal/LogOutWarningScreen.tsx
@@ -0,0 +1,30 @@
+import "./DeletionWarningScreen.css";
+import Modal from 'react-modal';
+
+type LogOutWarningScreenProps = {
+    modalIsOpen: boolean;
+    closeModal: () => void;
+    logOut: () => void;
+}
+
+export default function LogOutWarningScreen(props: LogOutWarningScreenProps) {
+    Modal.setAppElement('#root');
+    return (
+        <Modal
+            isOpen={props.modalIsOpen}
+            bodyOpenClassName={"has-open-modal"}
+            htmlOpenClassName={"has-open-modal"}
+            className={"modal deletion-warning"}
+        >
+            <h1>LOG OUT</h1>
+            <h2>
+                Do you want to log out of your account?<br/>
+                You can log in later again.
+            </h2>
+            <section className={"buttons"}>
+                <button className={"danger-button"} onClick={props.logOut}>Yes, log me out!</button>
+                <button className={"secondary-button"} onClick={props.closeModal}>Cancel</button>
+            </section>
+        </Modal>
+    );
+}
