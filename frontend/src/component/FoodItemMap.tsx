7ce4481fd1d47469a97bb589960265dc7557751a
valfink <117605562+valfink@users.noreply.github.com>
2023-04-11 12:44:39 +0200

    Add auto zoom for radius

--- frontend/src/component/FoodItemMap.tsx
+++ frontend/src/component/FoodItemMap.tsx
@@ -1,30 +1,38 @@
-import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet";
+import {Circle, MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet";
 import React, {useEffect} from "react";
 import {Location} from "../model/Location";
 import "./FoodItemMap.css";
 
 type FoodItemMapProps = {
     location: Location;
+    radius?: number;
 }
 
 function FoodItemMapController(props: FoodItemMapProps) {
     const map = useMap();
+
     useEffect(() => {
         map.setView([props.location.coordinate.latitude, props.location.coordinate.longitude]);
     }, [map, props.location.coordinate.latitude, props.location.coordinate.longitude]);
+
+    useEffect(() => {
+        if (props.radius) {
+            map.setZoom(23 - Math.floor(Math.log2(props.radius)));
+        }
+    }, [map, props.radius]);
 
     return <></>;
 }
 
 export default function FoodItemMap(props: FoodItemMapProps) {
     if (props.location.coordinate.latitude === 0) {
-        return <div className={"leaflet-container"}>Loading…</div>;
+        return <div className={"leaflet-container"}>Nothing to display yet…</div>;
     }
 
     return (
         <MapContainer center={[props.location.coordinate.latitude, props.location.coordinate.longitude]} zoom={15}
                       scrollWheelZoom={false}>
-            <FoodItemMapController location={props.location}/>
+            <FoodItemMapController location={props.location} radius={props.radius}/>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -34,6 +42,8 @@ export default function FoodItemMap(props: FoodItemMapProps) {
                     <strong>{props.location.title}</strong>
                 </Popup>
             </Marker>
+            {props.radius && <Circle radius={props.radius}
+                                     center={[props.location.coordinate.latitude, props.location.coordinate.longitude]}/>}
         </MapContainer>
     );
 }
