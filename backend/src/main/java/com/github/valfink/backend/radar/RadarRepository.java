d338f33aeab6c3acff7622baebb68d75d1006393
valfink <117605562+valfink@users.noreply.github.com>
2023-04-11 09:09:33 +0200

    Add existsByUserId method

--- backend/src/main/java/com/github/valfink/backend/radar/RadarRepository.java
+++ backend/src/main/java/com/github/valfink/backend/radar/RadarRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RadarRepository extends MongoRepository<Radar, String> {
+    boolean existsByUserId(String userId);
 }
