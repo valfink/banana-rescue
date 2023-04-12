72a604f55229a04fb3630545542ced017c08b9c4
valfink <117605562+valfink@users.noreply.github.com>
2023-04-12 11:35:24 +0200

    Add getRadar method

--- backend/src/main/java/com/github/valfink/backend/radar/RadarService.java
+++ backend/src/main/java/com/github/valfink/backend/radar/RadarService.java
@@ -1,30 +1,58 @@
 package com.github.valfink.backend.radar;
 
+import com.github.valfink.backend.fooditem.FoodItemDTOResponse;
+import com.github.valfink.backend.fooditem.FoodItemService;
 import com.github.valfink.backend.mongouser.MongoUserDTOResponse;
 import com.github.valfink.backend.mongouser.MongoUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.security.Principal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RadarService {
     private final RadarRepository radarRepository;
     private final MongoUserService mongoUserService;
+    private final FoodItemService foodItemService;
 
-    public RadarDTORequest addRadar(RadarDTORequest radarDTORequest, Principal principal) {
+    private List<FoodItemDTOResponse> getFoodItemsForRadar(Radar radar) {
+        return foodItemService
+                .getAllFoodItems()
+                .stream()
+                .filter(item ->
+                        item.location().coordinate().latitude().subtract(radar.center().latitude()).pow(2)
+                                .add(item.location().coordinate().longitude().subtract(radar.center().longitude()).pow(2))
+                                .compareTo(new BigDecimal(radar.radiusInMeters()).divide(new BigDecimal("111111"), RoundingMode.HALF_EVEN))
+                                <= 0
+                )
+                .toList();
+    }
+
+    public RadarDTOResponse addRadar(RadarDTORequest radarDTORequest, Principal principal) {
         MongoUserDTOResponse user = mongoUserService.getMongoUserDTOResponseByUsername(principal.getName());
 
-        if (radarRepository.existsByUserId(user.id())) {
+        if (radarRepository.existsById(user.id())) {
             throw new RadarExceptionBadInputData("You can only set one radar at a time.");
         }
         if (radarDTORequest.center() == null || radarDTORequest.center().latitude() == null || radarDTORequest.center().longitude() == null) {
             throw new RadarExceptionBadInputData("Center coordinate must be set.");
         }
 
-        Radar radar = radarRepository.save(new Radar(user.id(), radarDTORequest.center(), radarDTORequest.radiusInMeters()));
+        Radar radar = radarRepository.save(radarDTORequest.convertToRadar(user.id()));
 
-        return new RadarDTORequest(radar.center(), radar.radiusInMeters());
+        return radar.convertToDTOResponse(getFoodItemsForRadar(radar));
+    }
+
+    public RadarDTOResponse getRadar(Principal principal) {
+        MongoUserDTOResponse user = mongoUserService.getMongoUserDTOResponseByUsername(principal.getName());
+
+        Radar radar = radarRepository.findById(user.id()).orElse(null);
+        if (radar == null) return null;
+
+        return radar.convertToDTOResponse(getFoodItemsForRadar(radar));
     }
 }
--- backend/src/test/java/com/github/valfink/backend/radar/RadarServiceTest.java
+++ backend/src/test/java/com/github/valfink/backend/radar/RadarServiceTest.java
@@ -1,6 +1,9 @@
 package com.github.valfink.backend.radar;
 
+import com.github.valfink.backend.fooditem.FoodItemDTOResponse;
+import com.github.valfink.backend.fooditem.FoodItemService;
 import com.github.valfink.backend.geolocation.Coordinate;
+import com.github.valfink.backend.geolocation.Location;
 import com.github.valfink.backend.mongouser.MongoUserDTOResponse;
 import com.github.valfink.backend.mongouser.MongoUserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,30 +11,56 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.security.Principal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class RadarServiceTest {
     RadarRepository radarRepository;
     MongoUserService mongoUserService;
+    FoodItemService foodItemService;
     RadarService radarService;
     Principal principal;
-    MongoUserDTOResponse mongoUserDTOResponse1;
+    MongoUserDTOResponse mongoUserDTOResponse1, mongoUserDTOResponse2;
     Radar radar1;
     RadarDTORequest radarDTORequest1;
+    FoodItemDTOResponse foodItem1CloseBy, foodItem2NotSoClose;
 
     @BeforeEach
     void setUp() {
         radarRepository = mock(RadarRepository.class);
         mongoUserService = mock(MongoUserService.class);
-        radarService = new RadarService(radarRepository, mongoUserService);
+        foodItemService = mock(FoodItemService.class);
+        radarService = new RadarService(radarRepository, mongoUserService, foodItemService);
 
         principal = mock(Principal.class);
         mongoUserDTOResponse1 = new MongoUserDTOResponse("u1", "user");
+        mongoUserDTOResponse2 = new MongoUserDTOResponse("u2", "user2");
         radar1 = new Radar(mongoUserDTOResponse1.id(), new Coordinate(new BigDecimal("52.5170365"), new BigDecimal("13.3888599")), 200);
         radarDTORequest1 = new RadarDTORequest(radar1.center(), radar1.radiusInMeters());
+        foodItem1CloseBy = new FoodItemDTOResponse(
+                "f1",
+                mongoUserDTOResponse2,
+                "Food Item Close By",
+                null,
+                new Location("Very close", new Coordinate(new BigDecimal("52.51837"), new BigDecimal("13.38855"))),
+                Instant.parse("2023-04-12T11:30:00Z"),
+                Instant.parse("2023-04-14T12:00:00Z"),
+                "This is my food item close by."
+        );
+        foodItem2NotSoClose = new FoodItemDTOResponse(
+                "f2",
+                mongoUserDTOResponse2,
+                "Food Item Not So Close",
+                null,
+                new Location("Not So close", new Coordinate(new BigDecimal("52.51971"), new BigDecimal("13.38824"))),
+                Instant.parse("2023-04-12T11:30:00Z"),
+                Instant.parse("2023-04-14T12:00:00Z"),
+                "This is my food item not so close, but not very far away."
+        );
     }
 
     @Test
@@ -39,12 +68,13 @@ class RadarServiceTest {
         // GIVEN
         when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
         when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
-        when(radarRepository.existsByUserId(mongoUserDTOResponse1.id())).thenReturn(false);
+        when(radarRepository.existsById(mongoUserDTOResponse1.id())).thenReturn(false);
         when(radarRepository.save(radar1)).thenReturn(radar1);
+        when(foodItemService.getAllFoodItems()).thenReturn(List.of());
 
         // WHEN
-        RadarDTORequest expected = radarDTORequest1;
-        RadarDTORequest actual = radarService.addRadar(radarDTORequest1, principal);
+        RadarDTOResponse expected = radar1.convertToDTOResponse(List.of());
+        RadarDTOResponse actual = radarService.addRadar(radarDTORequest1, principal);
 
         // THEN
         verify(radarRepository).save(radar1);
@@ -56,7 +86,7 @@ class RadarServiceTest {
         // GIVEN
         when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
         when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
-        when(radarRepository.existsByUserId(mongoUserDTOResponse1.id())).thenReturn(true);
+        when(radarRepository.existsById(mongoUserDTOResponse1.id())).thenReturn(true);
 
         // WHEN & THEN
         assertThrows(RadarExceptionBadInputData.class, () -> radarService.addRadar(radarDTORequest1, principal));
@@ -67,7 +97,7 @@ class RadarServiceTest {
         // GIVEN
         when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
         when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
-        when(radarRepository.existsByUserId(mongoUserDTOResponse1.id())).thenReturn(false);
+        when(radarRepository.existsById(mongoUserDTOResponse1.id())).thenReturn(false);
         RadarDTORequest badRadarDTORequest = new RadarDTORequest(null, 100);
 
         // WHEN & THEN
@@ -79,7 +109,7 @@ class RadarServiceTest {
         // GIVEN
         when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
         when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
-        when(radarRepository.existsByUserId(mongoUserDTOResponse1.id())).thenReturn(false);
+        when(radarRepository.existsById(mongoUserDTOResponse1.id())).thenReturn(false);
         RadarDTORequest badRadarDTORequest = new RadarDTORequest(new Coordinate(null, new BigDecimal("50")), 100);
 
         // WHEN & THEN
@@ -91,10 +121,62 @@ class RadarServiceTest {
         // GIVEN
         when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
         when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
-        when(radarRepository.existsByUserId(mongoUserDTOResponse1.id())).thenReturn(false);
+        when(radarRepository.existsById(mongoUserDTOResponse1.id())).thenReturn(false);
         RadarDTORequest badRadarDTORequest = new RadarDTORequest(new Coordinate(new BigDecimal("50"), null), 100);
 
         // WHEN & THEN
         assertThrows(RadarExceptionBadInputData.class, () -> radarService.addRadar(badRadarDTORequest, principal));
+    }
+
+    @Test
+    void getRadar_whenUserHasRadar_thenReturnRadarDTO() {
+        // GIVEN
+        when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
+        when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
+        when(radarRepository.findById(mongoUserDTOResponse1.id())).thenReturn(Optional.of(radar1));
+        when(foodItemService.getAllFoodItems()).thenReturn(List.of());
+
+        // WHEN
+        RadarDTOResponse expected = radar1.convertToDTOResponse(List.of());
+        RadarDTOResponse actual = radarService.getRadar(principal);
+
+        // THEN
+        verify(radarRepository).findById(mongoUserDTOResponse1.id());
+        verify(foodItemService).getAllFoodItems();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getRadar_whenUserHasNoRadar_thenReturnNull() {
+        // GIVEN
+        when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
+        when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
+        when(radarRepository.findById(mongoUserDTOResponse1.id())).thenReturn(Optional.empty());
+
+        // WHEN
+        RadarDTOResponse actual = radarService.getRadar(principal);
+
+        // THEN
+        verify(radarRepository).findById(mongoUserDTOResponse1.id());
+        assertNull(actual);
+    }
+
+    @Test
+    void getRadar_whenUserHasRadarAndFoodItemIsCloseBy_thenReturnRadarDTOWithItem() {
+        // GIVEN
+        when(principal.getName()).thenReturn(mongoUserDTOResponse1.username());
+        when(mongoUserService.getMongoUserDTOResponseByUsername(mongoUserDTOResponse1.username())).thenReturn(mongoUserDTOResponse1);
+        when(radarRepository.findById(mongoUserDTOResponse1.id())).thenReturn(Optional.of(radar1));
+        when(foodItemService.getAllFoodItems()).thenReturn(List.of(foodItem1CloseBy, foodItem2NotSoClose));
+
+        // WHEN
+        RadarDTOResponse expected = radar1.convertToDTOResponse(List.of(foodItem1CloseBy));
+        // TODO: FIX THIS!
+        RadarDTOResponse actual = radarService.getRadar(principal);
+
+        // THEN
+        verify(radarRepository).findById(mongoUserDTOResponse1.id());
+        verify(foodItemService).getAllFoodItems();
+        assertEquals(expected, actual);
     }
 }
