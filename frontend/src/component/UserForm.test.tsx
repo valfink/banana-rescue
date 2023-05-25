68e52f7b7b06786c56b77397e97a2e89a873bb29
valfink <117605562+valfink@users.noreply.github.com>
2023-05-25 12:38:27 +0200

    Add first frontend test

---
+++ frontend/src/component/UserForm.test.tsx
@@ -0,0 +1,36 @@
+import {cleanup, render, screen} from "@testing-library/react";
+import UserForm from "./UserForm";
+import userEvent from "@testing-library/user-event";
+import {BrowserRouter} from "react-router-dom";
+import {UserContext, UserContextType} from "../context/UserContext";
+
+afterEach(cleanup);
+
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: () => jest.fn(),
+    useLocation: () => ({state: {redirectAfterLogIn: false}})
+}));
+const genericPromise = new Promise<void>(resolve => resolve());
+const mockedUserContext: UserContextType = {
+    user: undefined,
+    redirectIfNotSignedIn: () => undefined,
+    signUpUser: () => genericPromise,
+    logInUser: () => genericPromise,
+    logOutUser: () => genericPromise
+};
+
+describe("The user form", () => {
+    it("updates username state correctly", async () => {
+        render(
+            <BrowserRouter>
+                <UserContext.Provider value={mockedUserContext}>
+                    <UserForm action={"login"}/>
+                </UserContext.Provider>
+            </BrowserRouter>
+        );
+        const usernameTextbox = screen.getByPlaceholderText("Username");
+        await userEvent.type(usernameTextbox, "User_a");
+        expect(usernameTextbox).toHaveValue("User_a");
+    })
+});
